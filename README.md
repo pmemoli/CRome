1378c20fdbaac1e5642d349c12df9a77a2e50752
pmemoli <pedromemoli@hotmail.com>
2026-03-02 18:14:31 -0300

    Included lex, parse and codegen arg parsing.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 C compiler written in Rust based on Sandler Nora's book "Writing a C Compiler". 
 
+Preprocessor and Linker comes from gcc.
+
 Very much a WIP.
 
 TODO:
 
-- Implement file cleanup on error for the temp files of the compiler driver.
-- Handle --lex, --parse, and --codegen flags.
+- Implement file cleanup on error for the temp files from the compiler driver.
+- Handle --lex, --parse, and --codegen flags with clap rather than whatever i'm doing.
--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,33 @@ use std::process::Command;
 fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 2 {
-        return Err(Error::new(
-            ErrorKind::InvalidInput,
-            "Need a source file to compile.",
-        ));
+    let mut lex_flag = false;
+    let mut parse_flag = false;
+    let mut codegen_flag = false;
+    let mut file: Option<&str> = None;
+
+    for arg in args.iter().skip(1) {
+        match arg.as_str() {
+            "--lex" => lex_flag = true,
+            "--parse" => parse_flag = true,
+            "--codegen" => codegen_flag = true,
+            _ => file = Some(&arg),
+        }
     }
 
-    let source_file = &args[1];
+    let Some(file_name) = file else {
+        return Err(Error::new(
+            ErrorKind::InvalidInput,
+            "No source file provided.",
+        ));
+    };
 
     // Runs preprocessor
     let preprocessor_file_path = "./temp.i";
     let preprocessor_status = Command::new("gcc")
         .arg("-E") // Run only preprocessor
         .arg("-P") // No linemarkers
-        .arg(source_file)
+        .arg(file_name)
         .arg("-o")
         .arg(preprocessor_file_path)
         .status()?;
@@ -37,7 +49,7 @@ fn main() -> Result<(), std::io::Error> {
     fs::remove_file(preprocessor_file_path)?;
 
     // Runs linker
-    let output_file = source_file.strip_suffix(".c").unwrap_or(source_file);
+    let output_file = file_name.strip_suffix(".c").unwrap_or(file_name);
     let linker_status = Command::new("gcc")
         .arg(assembly_file_path)
         .arg("-o")
